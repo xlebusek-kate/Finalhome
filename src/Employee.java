1322c85f40928db88d42b8a02789703fdc500098
xlebusek-kate <iriskadeniska195@gmail.com>
2026-02-25 21:21:44 +0300

    Final

--- src/Employee.java
+++ src/Employee.java
@@ -4,14 +4,19 @@ public class Employee {
     private String informationAboutEmployee;
     private int department;
     private int salary;
-    private static int id;
+    private int id;
 
 
-    public Employee(int id, String informationAboutEmployee, int department, int salary) {
+    public Employee( String informationAboutEmployee, int department, int salary) {
 
         this.informationAboutEmployee = informationAboutEmployee;
         this.department = department;
         this.salary = salary;
+    }
+    public int setId(){
+       int idEmployee = 0;
+        idEmployee++;
+        return  idEmployee = id;
     }
 
     public int getDepartment() {
@@ -22,6 +27,9 @@ public class Employee {
         return this.informationAboutEmployee;
     }
 
+    public int getId(){
+        return this.id;
+    }
     public int getSalary() {
         return this.salary;
     }
@@ -34,8 +42,8 @@ public class Employee {
         return this.department = department;
     }
 
-    public void printShortInfo() {
-        System.out.println("Имя: " + informationAboutEmployee + "Зарплата: " + salary);
+    public String printShortInfo() {
+         return "Имя: " + informationAboutEmployee + "Зарплата: " + salary;
     }
 
     @Override
@@ -44,7 +52,7 @@ public class Employee {
         if (this == obj) return true;
         if (obj == null || getClass() != obj.getClass()) return false;
         Employee employee = (Employee) obj;
-        return salary == employee.salary && Objects.equals(salary, employee.salary);
+        return Objects.equals(salary, employee.salary);
     }
 
     public String toString() {
--- src/EmployeeBook.java
+++ src/EmployeeBook.java
@@ -1,25 +1,120 @@
-
-import java.util.Objects;
+import java.util.Arrays;
 
 public class EmployeeBook {
-    Employee[] employees=new Employee[10];
+    static final Employee[] employees = new Employee[10];
 
-    public String allEmployee(){
-        for(int i = 0;i<10 ;i ++ ){
-            if(employees[i] == null) break;
-            Objects.toString(employees[i]);// попытка вывести всю информацию, переделать
+    public static void fillingArrBook(Employee newEmployees) {
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] == null) {
+                employees[i] = newEmployees;
+                break;
+            }
         }
-        return "";
-    }
-    int allSalary = 0;
-    public void averageSalary(){
-        for(int o=0;; o++ ){
-
-
-        }
-
-
-
     }
 
+    public static void getEmployee() {
+        for (Employee employee : employees) {
+            if (employee != null) {
+                System.out.println(employee);
+            }
+        }
+    }
+
+    public static void printAllEmployees() {
+        System.out.println(Arrays.toString(employees));
+    }
+
+
+    int sum = 0;
+    int count = 0;
+
+    public int averageSalary() {
+        int sum = 0;
+        int count = 0;
+        for (int o = 0; ; o++) {
+            if (employees[o] != null) sum += employees[o].getSalary();
+            else break;
+            count++;
+        }
+        int averageSalaryEmployee = sum / count;
+        return averageSalaryEmployee;
+    }
+
+    public static void taxedSalary(String type) {
+        for (Employee employee : employees) {
+            if (employee == null) {
+                continue;
+            }
+            double salary = employee.getSalary();
+            double taxed = 0;
+
+            switch (type) {
+                case "PROPORTIONAL": {
+                    taxed = salary * 0.13;
+                    break;
+                }
+                case "PROGRESSIVE": {
+                    if (salary <= 150) {
+                        taxed = salary * 0.13;
+                        break;
+                    } else if (salary <= 350) {
+                        taxed = salary * 0.17;
+                        break;
+                    } else {
+                        taxed = salary * 0.21;
+                        break;
+                    }
+                }
+                default:
+                    System.out.println("Error");
+            }
+            System.out.println("Информация: " + employee.getInformationAboutEmployee() + ",индексация: " + taxed);
+            return;
+        }
+    }
+
+    public void indexSalary(int department, double taxed) {
+        for (Employee employee : employees) {
+            if (employee == null) continue;
+            if (employee.getDepartment() != department) continue;
+            int salary = (int) (employee.getSalary() * (1 + taxed / 100));
+            employee.setSalary(salary);
+            System.out.println(employee.getInformationAboutEmployee() + " итог: " + employee.getSalary());
+        }
+    }
+
+    public void findFirstEmployeeWithSalaryGreater(int department, int salary) {
+        for (int i = 0; i < employees.length; i++) {
+            Employee employee = employees[i];
+            if (employee == null) continue;
+            if (employee.getSalary() > salary && employee.getDepartment() == department) {
+
+                System.out.println("Место: " + (i + 1) + " -> " + employee.printShortInfo());
+            }
+        }
+    }
+
+    public void findFirstEmployeeWithSalaryLess(int wage, int empNumber) {
+        int i = 0;
+        int j = 0;
+        while (i < employees.length && j < empNumber) {
+            Employee employee = employees[i];
+            if (employee != null && employee.getSalary() < wage) {
+                employee.printShortInfo();
+                j++;
+            }
+            i++;
+            if (j == empNumber) break;
+        }
+    }
+
+
+    public boolean employeeBySalary(Employee employee) {
+        for (Employee employee1 : employees) {
+            if (employee != null && employee1.equals(employee)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
